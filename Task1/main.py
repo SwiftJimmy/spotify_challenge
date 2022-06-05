be82e7f42399811323dffc38e1cacbdbbede5ff7
Clemens Brauer <clemensbrauer@MBP-von-Clemens.fritz.box>
2022-06-05 22:05:13 +0200

    Update added loaded and invalide filepath

--- Task1/main.py
+++ Task1/main.py
@@ -1,19 +1,30 @@
 from watchdog.observers import Observer
-from libs.etl.etl import extract
+from libs.models.pipelinerunner import PipelineRunner
 from libs.models.observer import PipelineHandler
+import time
+
+DB_PATH = r'./database/spotify.db'
+INGEST_PATH = r'./data/ingest'
+INVALIDE_DATA_PATH = r'./data/invalide'
+FINISHED_DATA_PATH = r'./data/loaded'
 
 def main():
-    event_handler = PipelineHandler()
+    pipeline_runner = PipelineRunner(   db_path=DB_PATH, 
+                                        invalide_data_path=INVALIDE_DATA_PATH, 
+                                        loaded_data_path= FINISHED_DATA_PATH)
+    event_handler = PipelineHandler(pipeline_runner)
     observer = Observer()
-    observer.schedule(event_handler, path='./data/', recursive=False)
+    observer.schedule(event_handler, path=INGEST_PATH, recursive=False)
     observer.start()
 
-    while True:
-        try:
-            pass
-        except KeyboardInterrupt:
-            observer.stop()
-
+    try:
+        while True:
+            # Set the thread sleep time
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print('Stop')
+        observer.stop()
+    observer.join()
 
 
 if __name__ == '__main__':
